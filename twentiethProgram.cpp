4479cbcb0fab26531450037aa0c28a66679325fa
Diego-Camargo-Oropeza <desarrollo@kint.com.mx>
2025-04-04 22:56:40 -0600

    aa

--- twentiethProgram.cpp
+++ twentiethProgram.cpp
@@ -1,36 +1,111 @@
-//Program to code a mesage using queues and stacks
-//Example input: "Bond, James Bond" to "BnodJo s, dBneam"
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <string>
-#include <cctype>
-using namespace std;
+#include <stack>
+#include <queue>
 
-void reverseWords(string& s){
-    queue<char> q;
-    stack<char> st;
-    for (char c : s){
-        if (isalpha(c)){
-            q.push(c);
-        }
-    }
-    for (char c : s){
-        if (isalpha(c)){
-            st.push(q.front());
-            q.pop();
-        }
-    }
-    for (int i = 0; i < s.size(); i++){
-        if (isalpha(s[i])){
-            s[i] = st.top();
-            st.pop();
-        }
-    }
+using namespace std;\
+
+bool isVowel(char c) {
+    return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+            c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
 
-int main(){
+string transformStep1(string input) {
+    string output;
+    stack<char> S;
+    
+    for (char c : input) {
+        if (isVowel(c) || c == ' ') {
+            while (!S.empty()) {
+                output.push_back(S.top());
+                S.pop();
+            }
+            output.push_back(c);
+        } else {
+            S.push(c);
+        }
+    }
+    while (!S.empty()) {
+        output.push_back(S.top());
+        S.pop();
+    }
+    
+    return output;
+}
 
-    reverseWords("Bond, James Bond");
+string interleaveEnds(string Y) {
+    int n = Y.size();
+    queue<char> Q;
+    int i = 0, j = n - 1;
+    while (i <= j) {
+        Q.push(Y[i]);
+        if (i != j)
+            Q.push(Y[j]);
+        i++;
+        j--;
+    }
+    
+    string Z;
+    while (!Q.empty()) {
+        Z.push_back(Q.front());
+        Q.pop();
+    }
+    return Z;
+}
 
+string codeMessage(string X) {
+    string Y = transformStep1(X);
+    string Z = interleaveEnds(Y);
+    return Z;
+}
+
+string reverseInterleaveEnds(string Z) {
+    int n = Z.size();
+    string Y(n, ' ');
+    int left = 0, right = n - 1;
+    int index = 0;
+    while (left <= right && index < n) {
+        Y[left] = Z[index++];
+        if (left != right && index < n) {
+            Y[right] = Z[index++];
+        }
+        left++;
+        right--;
+    }
+    return Y;
+}
+
+string reverseTransformStep1(string Y) {
+    return transformStep1(Y);
+}
+
+string decodeMessage(string Z) {
+    string Y = reverseInterleaveEnds(Z);
+    string X = reverseTransformStep1(Y);
+    return X;
+}
+
+int main() {
+    int op;
+    string message;
+    
+    cout << "1. Code message\n";
+    cout << "2. Decode message\n";
+    cout << "Enter op: ";
+    cin >> op;
+    cin.ignore(); 
+    
+    if (op == 1) {
+        cout << "Enter the message to Code:\n";
+        getline(cin, message);
+        string coded = codeMessage(message);
+        cout << "Coded message:" << coded << "\n";
+    } else if (op == 2) {
+        cout << "Enter the message to decode:\n";
+        getline(cin, message);
+        string decoded = decodeMessage(message);
+        cout << "Decoded message:\n" << decoded << "\n";
+    }
+
+    return 0;
 }
